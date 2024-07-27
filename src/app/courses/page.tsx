d53c29fe2aa211a42e0033a182bb0298e366d379
Atam Swaroop <112094449+agamswaroop619@users.noreply.github.com>
2024-07-27 17:17:20 +0530

    add: Courses (with error)

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,10 +1,47 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 
-export default function Courses() {
+// Define the type for the group data
+interface Group {
+  id: number;
+  name: string;
+}
+
+const Courses: React.FC = () => {
+  const [groups, setGroups] = useState<Group[]>([]);
+
+  useEffect(() => {
+    fetchGroups();
+  }, []);
+
+  const fetchGroups = async () => {
+    try {
+      const response = await fetch(
+        "https://7e06fa9a-a9ed-469c-851e-094b33332263-00-30aaaf9celq3y.sisko.replit.dev/groups"
+      );
+      if (!response.ok) {
+        throw new Error(`Error fetching groups: ${response.statusText}`);
+      }
+      const data: Group[] = await response.json();
+      setGroups(data);
+    } catch (error) {
+      console.error("Error fetching groups:", error);
+    }
+  };
+
   return (
     <div>
       <Navbar />
-      <h1>Courses Page</h1>
+      <div>
+        {groups.length > 0 ? (
+          groups.map((group) => <p key={group.id}>{group.name}</p>)
+        ) : (
+          <p>Loading groups...</p>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default Courses;
